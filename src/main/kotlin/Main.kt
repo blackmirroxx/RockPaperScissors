3a47ec608c45344c68d056a25dc72fa526927363
olivier mutschler <ricardo@toppoint.de>
2022-07-19 22:04:03 +0200

    map -> rockPaperScissors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
-fun main(args: Array<String>) {
-    println("Welcome to Rock Paper Scissors!")
 
+
+fun main(args: Array<String>) {
+    println("Welcome to Rock Paper Scissors! ")
+
+    val rockPaperScissors = mapOf("rock" to 1, "paper" to 2, "scissors" to 3)
+
+    println("Enstries " + rockPaperScissors.entries)
+    println("Keys " + rockPaperScissors.keys)
+    println("Values" + rockPaperScissors.values)
 
 
 }
+
+
+// test
+
+
+// logic
