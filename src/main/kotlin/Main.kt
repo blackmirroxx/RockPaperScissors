f7e62d69c9e0e06b5a6c42d785f0069591328803
olivier mutschler <ricardo@toppoint.de>
2022-07-19 22:45:15 +0200

    map -> tests key value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,11 +9,50 @@ fun main(args: Array<String>) {
     println("Keys " + rockPaperScissors.keys)
     println("Values" + rockPaperScissors.values)
 
+    test_map_content(rockPaperScissors)
+
 
 }
 
 
 // test
+fun test_map_content(m: Map<String, Int>){
+    print("rock -> ")
+    if (m.get("rock") == 1) {
+        print("passed")
+    } else {
+        print("failed")
+    }
+    if (m.containsKey("rock")) {
+        println(" passed")
+    } else {
+        println(" failed")
+    }
+
+    print("paper -> ")
+    if (m.get("paper") == 2) {
+        print("passed")
+    } else {
+        print("failed")
+    }
+    if (m.containsKey("paper")) {
+        println(" passed")
+    } else {
+        println(" failed")
+    }
+
+    print("scissors -> ")
+    if (m.get("scissors") == 3) {
+        print("passed")
+    } else {
+        print("failed")
+    }
+    if (m.containsKey("scissors")) {
+        println(" passed")
+    } else {
+        println(" failed")
+    }
+}
 
 
 // logic
